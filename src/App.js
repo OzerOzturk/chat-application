8e04c8cbdd2b40a8087a0d0023ea9647e8af39bd
OzerOzturk <ozerozturk1636@gmail.com>
2021-11-20 16:20:35 +0300

    initial commit of chat app project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {ChatEngine} from 'react-chat-engine'
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height='100vh'
+            projectID='
+            ed2880d5-4637-4529-9628-dd921e7ce064'
+            userName = 'someonefromearth'
+            userSecret ='12345678'
+
+        
+        />
+    )
+}
+
+export default App;
